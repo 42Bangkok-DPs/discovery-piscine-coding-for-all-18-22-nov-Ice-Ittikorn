c9787f8dfe86a2d4579e572914050b8eae3ac0ab
Ice-Ittikorn <67030261@kmitl.ac.th>
2024-11-19 17:47:06 +0000

    ex

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,5 +1,7 @@
 import sys
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print("none")
 else:
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
     print(" ".join(str(i) for i in range(start, end + 1)))
