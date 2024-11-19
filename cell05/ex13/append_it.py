fc4ea80a491f6b146762b4f5b030447df2ee0ee4
Ice-Ittikorn <67030261@kmitl.ac.th>
2024-11-19 17:38:48 +0000

    ex

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -2,6 +2,6 @@ import sys
 if len(sys.argv) <2 :
     print("none")
 else:
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
         if not arg.endswith("ism"):
             print(f"{arg}ism")
