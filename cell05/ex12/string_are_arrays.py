bf922efb7e24872908e5041fe388909acbd01ba1
Ice-Ittikorn <67030261@kmitl.ac.th>
2024-11-19 17:29:54 +0000

    ex

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -4,4 +4,7 @@ if len(sys.argv) != 2:
 else:
     search = sys.argv[1]
     count_z = search.count('z')
-    print('z' * count_z)
+    if count_z == 0:
+        print("none")
+    else:
+        print('z' * count_z)
