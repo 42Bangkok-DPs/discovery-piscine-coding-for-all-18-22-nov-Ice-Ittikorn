6f1463247a7a090366a4e3f7f96cc17a62e22908
Ice-Ittikorn <67030261@kmitl.ac.th>
2024-11-21 17:15:32 +0000

    cascade

--- Rush00/ex01/main.py
+++ Rush00/ex01/main.py
@@ -1,14 +1,18 @@
+import sys
 from checkmate import checkmate
 
 def main():
-    board = """\
-....
-..K
-....
-....
-\
-"""
-    checkmate(board)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <file1.chess> [<file2.chess> ...]")
+        return
+
+    for filename in sys.argv[1:]:
+        try:
+            with open(filename, 'r') as file:
+                board = file.read()
+                checkmate(board)
+        except FileNotFoundError:
+            print(f"Error: File {filename} not found")
 
 if __name__ == "__main__":
     main()
